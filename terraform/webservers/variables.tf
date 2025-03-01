bf150836e2159e8db58d8bf8df3908c85d38c07c
swagatkoirala <swagatrajkoirala@gmail.com>
2025-03-01 13:22:07 -0500

    added the webservers terraform code and make sure to allow the traffice from nodeport 30000 in security groups

---
+++ terraform/webservers/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "Type of the instance"
+  type        = string
+}
+
+variable "prefix" {
+  default     = "assignment1"
+  description = "Prefix variable"
+  type        = string
+}
